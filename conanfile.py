fa187ec6f5457aa46ba9dd1d0d0b0a5c7b799e11
SechinM <m.sechin@avantize.com>
2016-11-28 16:46:52 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -23,10 +23,10 @@ class nanomsgConan(ConanFile):
         "enable_tests=False", \
         "enable_tools=True", \
         "enable_nanocat=True"
-    tools.download("http://download.redis.io/redis-stable.tar.gz", "redis-stable.tar.gz")
-    tools.untargz("redis-stable.tar.gz", "redis-stable")
     
     def source(self):
+        tools.download("http://download.redis.io/redis-stable.tar.gz", "redis-stable.tar.gz")
+        tools.untargz("redis-stable.tar.gz", "redis-stable")        
         self.run("cd redis-stable")
         
     def imports(self):
