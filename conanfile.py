2875db2bb250af5a93876ac3719700e8124d005a
SechinM <m.sechin@avantize.com>
2016-11-29 14:58:41 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -21,19 +21,19 @@ class redisConan(ConanFile):
         tools.download("http://download.redis.io/redis-stable.tar.gz", "redis-stable.tar.gz")
         tools.untargz("redis-stable.tar.gz", "redis-stable")       
         
-        cmake = CMake(self.settings)
+    #    cmake = CMake(self.settings)
 
-        cmake_options = []
-        for option_name in self.options.values.fields:
-            activated = getattr(self.options, option_name)
-            the_option = "%s=" % option_name.upper()
-            if option_name == "shared":
-                the_option = "REDIS_STATIC_LIB=OFF" if activated else "REDIS_STATIC_LIB=ON"
-            else:
-                the_option += "ON" if activated else "OFF"
-            cmake_options.append(the_option)
+    #    cmake_options = []
+    #    for option_name in self.options.values.fields:
+    #        activated = getattr(self.options, option_name)
+    #        the_option = "%s=" % option_name.upper()
+    #        if option_name == "shared":
+    #            the_option = "REDIS_STATIC_LIB=OFF" if activated else "REDIS_STATIC_LIB=ON"
+    #        else:
+    #            the_option += "ON" if activated else "OFF"
+    #        cmake_options.append(the_option)
 
-        cmake_cmd_options = " -D".join(cmake_options)
+    #    cmake_cmd_options = " -D".join(cmake_options)
                 
         #cmake_conf_command = 'cd redis-stable && make install %s' % (self.conanfile_directory, cmake.command_line, cmake_cmd_options)
         #self.output.warn(cmake_conf_command)
