b90b2a201006c0589f3b7440d295c1637d23fda9
m.sechin <m.sechin@avantize.com>
2016-12-01 13:39:25 +0200

    Updated conanfile, CMakeLists.

--- conanfile.py
+++ conanfile.py
@@ -9,18 +9,14 @@ class redisConan(ConanFile):
     url="https://github.com/SechinM/conan-redis.git"
     generators = "cmake", "txt"
     settings = "os", "compiler", "build_type", "arch"
-    short_paths = True
     options = {"shared": [True, False]}
     default_options = "shared=False"
+       
+    def source(self):
+        self.run("git clone https://github.com/Cylix/cpp_redis.git") 
+	os.mkdir("cpp_redis/build")
     
-    #def source(self):
-    #    tools.download("http://download.redis.io/redis-stable.tar.gz", "redis-stable.tar.gz")
-    #    tools.untargz("redis-stable.tar.gz", "redis-stable")           
-        
-    def build(self):          
-        tools.download("http://download.redis.io/redis-stable.tar.gz", "redis-stable.tar.gz")
-        tools.untargz("redis-stable.tar.gz", "redis-stable")       
-        
+    def build(self): 
         cmake = CMake(self.settings)
 
         cmake_options = []
@@ -28,41 +24,23 @@ class redisConan(ConanFile):
             activated = getattr(self.options, option_name)
             the_option = "%s=" % option_name.upper()
             if option_name == "shared":
-                the_option = "REDIS_STATIC_LIB=OFF" if activated else "REDIS_STATIC_LIB=ON"
+                the_option = "CPP_REDIS_STATIC_LIB=OFF" if activated else "CPP_REDIS_STATIC_LIB=ON"
             else:
                 the_option += "ON" if activated else "OFF"
             cmake_options.append(the_option)
 
         cmake_cmd_options = " -D".join(cmake_options)
                 
-        #cmake_conf_command = 'cd redis-stable && make install %s' % (self.conanfile_directory, cmake.command_line, cmake_cmd_options)
-        #self.output.warn(cmake_conf_command)
-        #self.run(cmake_conf_command)
-        #self.run("cd redis-stable && make install")# % cmake.build_config)
-        
-        
-        #cmake_conf_command = 'cmake %s/redis-stable %s -DCMAKE_INSTALL_PREFIX:PATH=install -D%s' % (self.conanfile_directory, cmake.command_line, cmake_cmd_options)
-        #self.output.warn(cmake_conf_command)
-        #self.run(cmake_conf_command)
+        self.run("cd cpp_redis/build && cmake -DCMAKE_INSTALL_PREFIX=/usr/local ..")
+        self.run("cd cpp_redis/build && make")
+        self.run("cd cpp_redis/build && make install")
 
-        self.run("make install %s" % cmake.build_config)
     
-    #def imports(self):
-    #    self.copy("*.dll", dst="bin", src="bin")
-    #    self.copy("*.dylib*", dst="bin", src="lib")
-
     def package(self):
-        self.copy("*.h", dst="include", src="install/include")
-        self.copy("*.dll", dst="bin", src="install/bin")
-        self.copy("*.lib", dst="lib", src="install/lib")
-        self.copy("*.a", dst="lib", src="install/lib")
-        self.copy("*.so*", dst="lib", src="install/lib")
-        #self.copy("*.dylib", dst="lib", src="install/lib")
-        #self.copy("nanocat*", dst="bin", src="install/bin")
-        #self.copy("*.*", dst="lib/pkgconfig", src="install/lib/pkgconfig")
-
+        self.copy("*.h", dst="include", src="/install/include")
+        self.copy("*.hpp", dst="include", src="/install/include")
+        self.copy("*.a", dst="lib", src="/install/lib")
+        self.copy("*.so", dst="lib", src="/install/lib")
+ 
     def package_info(self):
-        self.cpp_info.libs = ["redis"]
-
-        if not self.options.shared:
-            self.cpp_info.defines.extend(["REDIS_STATIC_LIB=ON"])
+        self.cpp_info.libs = ["cpp_redis"]
---
+++ test_package/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cpp_redis/cpp_redis>
+
+int main(int argc, char *argv[])
+{
+cpp_redis::redis_client client_;
+
+	 try{
+	client_.connect("127.0.0.1", 6379, [] (cpp_redis::redis_client &) {
+	  std::cout << "client disconnected (disconnection handler)" << std::endl;
+	});
+	}
+	catch (std::exception const& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
