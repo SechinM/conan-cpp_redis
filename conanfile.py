bbcad67f9ad92c9a9480a8d2d2de506f39f7704a
m.sechin <m.sechin@avantize.com>
2016-12-16 17:13:18 +0200

    Connan

--- conanfile.py
+++ conanfile.py
@@ -9,12 +9,12 @@ class redisConan(ConanFile):
     url="https://github.com/SechinM/conan-redis.git"
     generators = "cmake", "txt"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+#    options = {"shared": [True, False]}
+#    default_options = "shared=True"
        
     def source(self):
         self.run("git clone https://github.com/Cylix/cpp_redis.git")
-        self.run("cd cpp_redis && git checkout 8f902dae91d87fa0be891d40e5c853762dae6060")  
+#        self.run("cd cpp_redis && git checkout 8f902dae91d87fa0be891d40e5c853762dae6060")  
     
     def build(self): 
         cmake = CMake(self.settings)
@@ -23,19 +23,34 @@ class redisConan(ConanFile):
         for option_name in self.options.values.fields:
             activated = getattr(self.options, option_name)
             the_option = "%s=" % option_name.upper()
-            if option_name == "shared":
-                the_option = "CPP_REDIS_STATIC_LIB=OFF" if activated else "CPP_REDIS_STATIC_LIB=ON"
-            else:
-                the_option += "ON" if activated else "OFF"
+#            if option_name == "shared":
+#                the_option = "CPP_REDIS_SHARED_LIB=OFF" if activated else "CPP_REDIS_SHARED_LIB=ON"
+#            else:
+#                the_option += "ON" if activated else "OFF"
             cmake_options.append(the_option)
 
-        cmake_cmd_options = " -D".join(cmake_options)
+       	cmake_cmd_options = " -D".join(cmake_options)
                 
-	cmake_conf_command = 'cmake %s/cpp_redis %s -DCMAKE_INSTALL_PREFIX:PATH=install -D%s' % (self.conanfile_directory, cmake.command_line, cmake_cmd_options)
+#	cmake_conf_command = 'cmake %s/cpp_redis %s -DCMAKE_INSTALL_PREFIX:PATH=install .. -D%s' % (self.conanfile_directory, cmake.command_line, cmake_cmd_options)
+#        self.output.warn(cmake_conf_command)
+#        self.run(cmake_conf_command)
+
+
+        cmake_conf_command = 'cmake %s/cpp_redis %s -DCMAKE_INSTALL_PREFIX:PATH=install%s' % (self.conanfile_directory, cmake.command_line, cmake_cmd_options)
         self.output.warn(cmake_conf_command)
         self.run(cmake_conf_command)
-
+	self.run("cmake --build . %s" % cmake.build_config)
 	self.run("cmake --build . --target install %s" % cmake.build_config)
+
+#        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+#        self.run('cmake --build . %s' % cmake.build_config)
+#        self.run('cmake --build . --target install %s' % cmake.build_config)
+
+
+#	self.run("cd cpp_redis && mkdir build")
+#	self.run("cd cpp_redis/build && cmake ..")	
+#	self.run("cd cpp_redis/build make")
+#	self.run("cd cpp_redis/build make install")
 
 
         #self.run("cd cpp_redis/build && sudo cmake -DCMAKE_INSTALL_PREFIX=/usr/local ..")
